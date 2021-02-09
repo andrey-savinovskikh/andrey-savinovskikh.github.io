04bd378ce3901ea5571cf8fe467438629f9f1736
Andrey Savinovskikh <andrey.savinovskikh@gmail.com>
2021-02-09 21:33:27 +0700

    Init

---
+++ precache-manifest.392e27c3cca7fd64b43c8c0aeb0cadb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c391bfff05374f1a54eb9a6a6697e48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d4384d8ca63abae748c",
+    "url": "./static/css/main.d7fced75.chunk.css"
+  },
+  {
+    "revision": "427d7b8947b06fca8c74",
+    "url": "./static/js/2.4a541eef.chunk.js"
+  },
+  {
+    "revision": "e465a1d90cd90ed95cee5d0fa63a8915",
+    "url": "./static/js/2.4a541eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4384d8ca63abae748c",
+    "url": "./static/js/main.e6b7c1f0.chunk.js"
+  },
+  {
+    "revision": "7d445abe9f11439e6ca4",
+    "url": "./static/js/runtime-main.aedeedb6.js"
+  }
+]);
